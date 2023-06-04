42214dfff9e113c299127561b822365940733686
Silva <jrjrsilva@gmail.com>
2023-06-04 17:21:16 -0300

    Finalizando Blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,21 @@
+export const dataFake = [
+
+    {
+        "id": "1",
+        "title": "1ª Temporada",
+        "description": "description 1ª Temporada",
+        "photoCover": "https://img.ibxk.com.br/2020/12/11/11133657574178.jpg"
+    },
+    {
+        "id": "2",
+        "title": "2ª Temporada",
+        "description": "description 2ª Temporada",
+        "photoCover": "https://images.justwatch.com/backdrop/221611732/s1440/temporada-1.webp"
+    },
+    {
+        "id": "3",
+        "title": "3ª Temporada",
+        "description": "description da 3ª Temporada",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/04/mandalorian_temporada_3_critica__3nr04105-1210x544.jpg"
+    }
+]
